1efa7b4983461fa8fe80b93a3dd1781b06c21fe5
SKS <shaha460@gmail.com>
2021-07-31 22:48:11 +0600

    actions types set seletected product

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -2,7 +2,14 @@ import { ActionTypes } from "../contants/action-type";
 
 export const SetProduct = (product) =>{
     return {
-        type: ActionTypes.SELECTED_PRODUCT,
+        type: ActionTypes.SET_PRODUCT,
         payload: product
+    }
+}
+
+export const SelectedProduct = (products) =>{
+    return {
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: products
     }
 }
